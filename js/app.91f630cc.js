2cf44ee064ee07dab3899c33b8bc6f569f48a29e
Xinyu Fu <2013201239@ruc.edu.cn>
2020-10-22 16:10:11 +0800

    Update app.91f630cc.js
    enhanced mindfulness

--- js/app.91f630cc.js
+++ js/app.91f630cc.js
@@ -1375,6 +1375,18 @@
                         },
                         expression: "explanation"
                     }
+                }), [e._v("Another task:")]), i("p", [e._v("Why do you think the bot made an error in assessing comment #3 and $4 incorrectly?")]), i("el-input", {
+                    attrs: {
+                        type: "textarea",
+                        rows: 4
+                    },
+                    model: {
+                        value: e.explanation,
+                        callback: function(t) {
+                            e.explanation = t
+                        },
+                        expression: "explanation"
+                    }
                 }), !1 === e.isFinished ? i("p", {
                     staticClass: "error"
                 }, [e._v("Min length is " + e._s(e.minLength) + " characters, " + e._s(e.remainingLength) + " remaining.")]) : e._e()], 1)])
