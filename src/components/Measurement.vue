d471f06707bc06037d26a7e5cbecf3bd8fb47f67
程康 <me@chengkang.pw>
2022-03-27 16:20:48 -0700

    feat:: update measurement

--- src/components/Measurement.vue
+++ src/components/Measurement.vue
@@ -2,55 +2,129 @@
 <template>
   <article>
     <h1>
-      In the last six months, for each news article you read, how often did
-      you...
+      Please rate your experience engaging with the discussion forum and the
+      moderation bot.
     </h1>
-    <table>
+    <table style="border-spacing: 1.2em">
+      <thead>
+        <tr>
+          <td></td>
+          <td>Strongly disagree</td>
+          <td>Somewhat disagree</td>
+          <td>Neither agree nor disagree</td>
+          <td>Somewhat agree</td>
+          <td>Strongly agree</td>
+        </tr>
+      </thead>
       <tbody>
         <tr>
-          <td>read the comments section</td>
-          <td><el-radio v-model="q1" label="Never"></el-radio></td>
-          <td><el-radio v-model="q1" label="Seldom"></el-radio></td>
-          <td><el-radio v-model="q1" label="Sometimes"></el-radio></td>
-          <td><el-radio v-model="q1" label="Often"></el-radio></td>
-          <td><el-radio v-model="q1" label="Usually"></el-radio></td>
-          <td><el-radio v-model="q1" label="Almost every time"></el-radio></td>
+          <td>
+            When I need to review a large volume of comments, it makes sense to
+            delegate the moderation task to a bot.
+          </td>
+          <td><el-radio v-model="q1" label="1"></el-radio></td>
+          <td><el-radio v-model="q1" label="2"></el-radio></td>
+          <td><el-radio v-model="q1" label="3"></el-radio></td>
+          <td><el-radio v-model="q1" label="4"></el-radio></td>
+          <td><el-radio v-model="q1" label="5"></el-radio></td>
         </tr>
         <tr>
-          <td>leave a comment</td>
-          <td><el-radio v-model="q2" label="Never"></el-radio></td>
-          <td><el-radio v-model="q2" label="Seldom"></el-radio></td>
-          <td><el-radio v-model="q2" label="Sometimes"></el-radio></td>
-          <td><el-radio v-model="q2" label="Often"></el-radio></td>
-          <td><el-radio v-model="q2" label="Usually"></el-radio></td>
-          <td><el-radio v-model="q2" label="Almost every time"></el-radio></td>
+          <td>
+            When visiting an online forum, if I were looking for certain
+            information, I would let the bot handle some moderation tasks for
+            me.
+          </td>
+          <td><el-radio v-model="q2" label="1"></el-radio></td>
+          <td><el-radio v-model="q2" label="2"></el-radio></td>
+          <td><el-radio v-model="q2" label="3"></el-radio></td>
+          <td><el-radio v-model="q2" label="4"></el-radio></td>
+          <td><el-radio v-model="q2" label="5"></el-radio></td>
         </tr>
         <tr>
-          <td>reply to others' comments</td>
-          <td><el-radio v-model="q3" label="Never"></el-radio></td>
-          <td><el-radio v-model="q3" label="Seldom"></el-radio></td>
-          <td><el-radio v-model="q3" label="Sometimes"></el-radio></td>
-          <td><el-radio v-model="q3" label="Often"></el-radio></td>
-          <td><el-radio v-model="q3" label="Usually"></el-radio></td>
-          <td><el-radio v-model="q3" label="Almost every time"></el-radio></td>
+          <td>The bot should be used to ease users’ workload.</td>
+          <td><el-radio v-model="q3" label="1"></el-radio></td>
+          <td><el-radio v-model="q3" label="2"></el-radio></td>
+          <td><el-radio v-model="q3" label="3"></el-radio></td>
+          <td><el-radio v-model="q3" label="4"></el-radio></td>
+          <td><el-radio v-model="q3" label="5"></el-radio></td>
         </tr>
         <tr>
-          <td>entered (thumbs up) or (thumbs down) for posts</td>
-          <td><el-radio v-model="q4" label="Never"></el-radio></td>
-          <td><el-radio v-model="q4" label="Seldom"></el-radio></td>
-          <td><el-radio v-model="q4" label="Sometimes"></el-radio></td>
-          <td><el-radio v-model="q4" label="Often"></el-radio></td>
-          <td><el-radio v-model="q4" label="Usually"></el-radio></td>
-          <td><el-radio v-model="q4" label="Almost every time"></el-radio></td>
+          <td>
+            If the bot is available to help me with moderating comments, it
+            makes sense for me to shift more attention to my other activities on
+            the discussion forum (e.g., leave comments and replies to other
+            users, click thumbs up or dislike for threads).
+          </td>
+          <td><el-radio v-model="q4" label="1"></el-radio></td>
+          <td><el-radio v-model="q4" label="2"></el-radio></td>
+          <td><el-radio v-model="q4" label="3"></el-radio></td>
+          <td><el-radio v-model="q4" label="4"></el-radio></td>
+          <td><el-radio v-model="q4" label="5"></el-radio></td>
         </tr>
         <tr>
-          <td>flat/report an inappropriate comment or reply</td>
-          <td><el-radio v-model="q5" label="Never"></el-radio></td>
-          <td><el-radio v-model="q5" label="Seldom"></el-radio></td>
-          <td><el-radio v-model="q5" label="Sometimes"></el-radio></td>
-          <td><el-radio v-model="q5" label="Often"></el-radio></td>
-          <td><el-radio v-model="q5" label="Usually"></el-radio></td>
-          <td><el-radio v-model="q5" label="Almost every time"></el-radio></td>
+          <td>
+            Even if the bot can help me with the moderation task, I should pay
+            attention to its performance.
+          </td>
+          <td><el-radio v-model="q5" label="1"></el-radio></td>
+          <td><el-radio v-model="q5" label="2"></el-radio></td>
+          <td><el-radio v-model="q5" label="3"></el-radio></td>
+          <td><el-radio v-model="q5" label="4"></el-radio></td>
+          <td><el-radio v-model="q5" label="5"></el-radio></td>
+        </tr>
+        <tr>
+          <td>
+            Distractions and interruptions are less of a problem for me when I
+            have a bot to cover the moderation task.
+          </td>
+          <td><el-radio v-model="q5" label="1"></el-radio></td>
+          <td><el-radio v-model="q5" label="2"></el-radio></td>
+          <td><el-radio v-model="q5" label="3"></el-radio></td>
+          <td><el-radio v-model="q5" label="4"></el-radio></td>
+          <td><el-radio v-model="q5" label="5"></el-radio></td>
+        </tr>
+        <tr>
+          <td>
+            Constantly monitoring the bot’s performance is a waste of time.
+          </td>
+          <td><el-radio v-model="q5" label="1"></el-radio></td>
+          <td><el-radio v-model="q5" label="2"></el-radio></td>
+          <td><el-radio v-model="q5" label="3"></el-radio></td>
+          <td><el-radio v-model="q5" label="4"></el-radio></td>
+          <td><el-radio v-model="q5" label="5"></el-radio></td>
+        </tr>
+        <tr>
+          <td>
+            Even when I have a lot to do, I am likely to watch the bot carefully
+            for errors.
+          </td>
+          <td><el-radio v-model="q5" label="1"></el-radio></td>
+          <td><el-radio v-model="q5" label="2"></el-radio></td>
+          <td><el-radio v-model="q5" label="3"></el-radio></td>
+          <td><el-radio v-model="q5" label="4"></el-radio></td>
+          <td><el-radio v-model="q5" label="5"></el-radio></td>
+        </tr>
+        <tr>
+          <td>
+            It’s not 5 necessary to pay much attention to the bot when it
+            catches the potential abusive comments on the forum.
+          </td>
+          <td><el-radio v-model="q5" label="1"></el-radio></td>
+          <td><el-radio v-model="q5" label="2"></el-radio></td>
+          <td><el-radio v-model="q5" label="3"></el-radio></td>
+          <td><el-radio v-model="q5" label="4"></el-radio></td>
+          <td><el-radio v-model="q5" label="5"></el-radio></td>
+        </tr>
+        <tr>
+          <td>
+            Carefully watching the bot takes time away from more important or
+            interesting things.
+          </td>
+          <td><el-radio v-model="q5" label="1"></el-radio></td>
+          <td><el-radio v-model="q5" label="2"></el-radio></td>
+          <td><el-radio v-model="q5" label="3"></el-radio></td>
+          <td><el-radio v-model="q5" label="4"></el-radio></td>
+          <td><el-radio v-model="q5" label="5"></el-radio></td>
         </tr>
       </tbody>
     </table>
@@ -69,11 +143,27 @@ export default {
       q3: undefined,
       q4: undefined,
       q5: undefined,
+      q6: undefined,
+      q7: undefined,
+      q8: undefined,
+      q9: undefined,
+      q10: undefined,
     };
   },
   computed: {
     isFinished() {
-      return this.q1 && this.q2 && this.q3 && this.q4 && this.q5;
+      return (
+        this.q1 &&
+        this.q2 &&
+        this.q3 &&
+        this.q4 &&
+        this.q5 &&
+        this.q6 &&
+        this.q7 &&
+        this.q8 &&
+        this.q9 &&
+        this.q10
+      );
     },
   },
   watch: {
@@ -85,6 +175,11 @@ export default {
           q3: this.q3,
           q4: this.q4,
           q5: this.q5,
+          q6: this.q6,
+          q7: this.q7,
+          q8: this.q8,
+          q9: this.q9,
+          q10: this.q10,
         };
 
         emitter.emit("step-finished", {
@@ -106,5 +201,8 @@ article {
   font-size: 17px;
   line-height: 1.4em;
   padding: 24px;
+}
+h1 {
+  line-height: 1.4;
 }
 </style>
